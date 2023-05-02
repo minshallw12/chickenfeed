14e74e384fa51b5c73a46bdceb39a24804601935
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-05-02 13:01:49 -0600

    initial commit

---
+++ chickenfeed/chickenfeedback/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def send_the_index(request):
+    the_index = open('static/index.html')
+    return HttpResponse(the_index)
